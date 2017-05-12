914c70e58f823812a29692fcac8af516252e895c
noisyfox <timemanager.rick@gmail.com>
2017-05-12 13:38:28 +1000

    Add non-coroutine count() and infinite repeat()

--- src/main/kotlin/cn/noisyfox/kotlin/itertools/Infinite.kt
+++ src/main/kotlin/cn/noisyfox/kotlin/itertools/Infinite.kt
@@ -22,6 +22,117 @@ package cn.noisyfox.kotlin.itertools
  */
 
 /**
+ * Make an iterator that returns values starting with [start] with evenly space [step].
+ */
+inline fun <T> count(start: T, step: T, crossinline add: (T, T) -> T): Sequence<T> {
+    return Sequence {
+        object : Iterator<T> {
+            var n = start
+
+            override fun hasNext(): Boolean = true
+
+            override fun next(): T {
+                val v = n
+                n = add(n, step)
+
+                return v
+            }
+        }
+    }
+}
+
+/**
+ * Make an iterator that returns values starting with [start] with evenly space [step].
+ */
+fun count(start: Byte, step: Byte = 1): Sequence<Byte> = count(start, step) { n, s -> (n + s).toByte() }
+
+/**
+ * Make an iterator that returns values starting with [start] with evenly space [step].
+ */
+fun count(start: Short, step: Short = 1): Sequence<Short> = count(start, step) { n, s -> (n + s).toShort() }
+
+/**
+ * Make an iterator that returns values starting with [start] with evenly space [step].
+ */
+fun count(start: Int, step: Int = 1): Sequence<Int> = count(start, step) { n, s -> n + s }
+
+/**
+ * Make an iterator that returns values starting with [start] with evenly space [step].
+ */
+fun count(start: Long, step: Long = 1L): Sequence<Long> = count(start, step) { n, s -> n + s }
+
+/**
+ * Make an iterator that returns values starting with [start] with evenly space [step].
+ */
+fun count(start: Float, step: Float = 1f): Sequence<Float> = count(start, step) { n, s -> n + s }
+
+/**
+ * Make an iterator that returns values starting with [start] with evenly space [step].
+ */
+fun count(start: Double, step: Double = 1.0): Sequence<Double> = count(start, step) { n, s -> n + s }
+
+
+/**
+ * Make an iterator returning elements from the iterable and saving a copy of each.
+ * When the iterable is exhausted, return elements from the saved copy. Repeats indefinitely.
+ */
+fun <T> Iterable<T>.cycle(): Sequence<T> {
+    val notEmpty = this.any()
+
+    return Sequence {
+        object : Iterator<T> {
+            val saved = mutableListOf<T>()
+            var iter: Iterator<T>? = this@cycle.iterator()
+            lateinit var iter_saved: Iterator<T>
+
+            override fun hasNext(): Boolean = notEmpty
+
+            override fun next(): T {
+                val iter_orig = iter
+
+                if (iter_orig != null) {
+                    if (iter_orig.hasNext()) {
+                        val v = iter_orig.next()
+                        saved.add(v)
+
+                        return v
+                    } else {
+                        iter = null
+
+                        if (saved.none()) {
+                            throw IllegalStateException("Items changed during iterating.")
+                        }
+
+                        iter_saved = saved.iterator()
+                    }
+                }
+
+                if (!iter_saved.hasNext()) {
+                    iter_saved = saved.iterator()
+                }
+
+                return iter_saved.next()
+            }
+
+        }
+    }
+}
+
+
+/**
+ * Make an iterator that returns [element] over and over again. Runs indefinitely.
+ */
+fun <T> repeat(element: T): Sequence<T> {
+    return Sequence {
+        object : Iterator<T> {
+            override fun hasNext(): Boolean = true
+
+            override fun next(): T = element
+        }
+    }
+}
+
+/**
  * Make an iterator that returns [element] over and over again. Runs [count] times.
  */
 fun <T> repeat(element: T, count: Int): Sequence<T> {
--- src/test/kotlin/cn/noisyfox/kotlin/itertools/test/ExtensionMethodTests.kt
+++ src/test/kotlin/cn/noisyfox/kotlin/itertools/test/ExtensionMethodTests.kt
@@ -113,6 +113,31 @@ class TestExtensionMethods {
     }
 
     @test fun testInfinite() {
+        val byte_start: Byte = 1
+        val byte_step: Byte = 2
+        assertEquals(listOf<Byte>(1, 2, 3, 4, 5, 6), count(byte_start).take(6).toList())
+        assertEquals(listOf<Byte>(1, 3, 5, 7, 9, 11), count(byte_start, byte_step).take(6).toList())
+
+        val short_start: Short = 1
+        val short_step: Short = 2
+        assertEquals(listOf<Short>(1, 2, 3, 4, 5, 6), count(short_start).take(6).toList())
+        assertEquals(listOf<Short>(1, 3, 5, 7, 9, 11), count(short_start, short_step).take(6).toList())
+
+        assertEquals(listOf(1, 2, 3, 4, 5, 6), count(1).take(6).toList())
+        assertEquals(listOf(1, 3, 5, 7, 9, 11), count(1, 2).take(6).toList())
+
+        assertEquals(listOf<Long>(1, 2, 3, 4, 5, 6), count(1L).take(6).toList())
+        assertEquals(listOf<Long>(1, 3, 5, 7, 9, 11), count(1L, 2L).take(6).toList())
+
+        assertEquals(listOf(1f, 2f, 3f, 4f, 5f, 6f), count(1f).take(6).toList())
+        assertEquals(listOf(1f, 3f, 5f, 7f, 9f, 11f), count(1f, 2f).take(6).toList())
+
+        assertEquals(listOf(1.0, 2.0, 3.0, 4.0, 5.0, 6.0), count(1.0).take(6).toList())
+        assertEquals(listOf(1.0, 3.0, 5.0, 7.0, 9.0, 11.0), count(1.0, 2.0).take(6).toList())
+
+        assertEquals(listOf(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4), listOf(1, 2, 3, 4).cycle().take(12).toList())
+
+        assertEquals(listOf(1, 1, 1, 1, 1, 1, 1, 1), repeat(1).take(8).toList())
         assertEquals(listOf(1, 1, 1, 1, 1, 1, 1, 1), repeat(1, 8).toList())
     }
 }
